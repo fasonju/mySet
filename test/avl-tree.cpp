d87fc9c8546b5b5a9b37ef30ba6b5ecf3f63311b
jasonfu <gimpact709@gmail.com>
2025-06-06 21:12:51 +0200

    leafremoval

--- test/avl-tree.cpp
+++ test/avl-tree.cpp
@@ -4,33 +4,33 @@
 #include <gtest/gtest.h>
 
 TEST(AvlTree, Initalization) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    EXPECT_TRUE(tree->height() == 0);
-    EXPECT_TRUE(tree->empty());
-    EXPECT_TRUE(tree->min() == nullptr);
-    EXPECT_TRUE(tree->max() == nullptr);
-    EXPECT_TRUE(tree->size() == 0);
+    EXPECT_TRUE(tree.height() == 0);
+    EXPECT_TRUE(tree.empty());
+    EXPECT_TRUE(tree.min() == nullptr);
+    EXPECT_TRUE(tree.max() == nullptr);
+    EXPECT_TRUE(tree.size() == 0);
 }
 
 TEST(AvlTree, SimpleInsertion) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    const bool succeeded = tree->insert(1);
-    const int *value = tree->search(1);
+    const bool succeeded = tree.insert(1);
+    const int *value = tree.search(1);
 
     EXPECT_TRUE(succeeded);
     EXPECT_TRUE(*value == 1);
 }
 
 TEST(AvlTree, DuplicateKeyInsertion) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(1);
-    const bool succeeded = tree->insert(1);
-    const int *value = tree->search(1);
-    const int height = tree->height();
-    const size_t size = tree->size();
+    tree.insert(1);
+    const bool succeeded = tree.insert(1);
+    const int *value = tree.search(1);
+    const int height = tree.height();
+    const size_t size = tree.size();
 
     EXPECT_FALSE(succeeded);
     EXPECT_TRUE(*value == 1);
@@ -39,17 +39,17 @@ TEST(AvlTree, DuplicateKeyInsertion) {
 }
 
 TEST(AvlTree, LeftLeftInsertion) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(4);
-    tree->insert(5);
-    tree->insert(3);
-    tree->insert(2);
-    const int preRotationHeight = tree->height();
-    const size_t preRotationSize = tree->size();
-    tree->insert(1);
-    const int postRotationHeight = tree->height();
-    const size_t postRotationSize = tree->size();
+    tree.insert(4);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(2);
+    const int preRotationHeight = tree.height();
+    const size_t preRotationSize = tree.size();
+    tree.insert(1);
+    const int postRotationHeight = tree.height();
+    const size_t postRotationSize = tree.size();
 
     EXPECT_TRUE(preRotationHeight == 3);
     EXPECT_TRUE(preRotationSize == 4);
@@ -58,17 +58,17 @@ TEST(AvlTree, LeftLeftInsertion) {
 }
 
 TEST(AvlTree, RightRightInsertion) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(2);
-    tree->insert(1);
-    tree->insert(3);
-    tree->insert(4);
-    const int preRotationHeight = tree->height();
-    const size_t preRotationSize = tree->size();
-    tree->insert(5);
-    const int postRotationHeight = tree->height();
-    const size_t postRotationSize = tree->size();
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(3);
+    tree.insert(4);
+    const int preRotationHeight = tree.height();
+    const size_t preRotationSize = tree.size();
+    tree.insert(5);
+    const int postRotationHeight = tree.height();
+    const size_t postRotationSize = tree.size();
 
     EXPECT_TRUE(preRotationHeight == 3);
     EXPECT_TRUE(preRotationSize == 4);
@@ -77,17 +77,17 @@ TEST(AvlTree, RightRightInsertion) {
 }
 
 TEST(AvlTree, LeftRightInsertion) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(6);
-    tree->insert(7);
-    tree->insert(5);
-    tree->insert(3);
-    const int preRotationHeight = tree->height();
-    const size_t preRotationSize = tree->size();
-    tree->insert(4);
-    const int postRotationHeight = tree->height();
-    const size_t postRotationSize = tree->size();
+    tree.insert(6);
+    tree.insert(7);
+    tree.insert(5);
+    tree.insert(3);
+    const int preRotationHeight = tree.height();
+    const size_t preRotationSize = tree.size();
+    tree.insert(4);
+    const int postRotationHeight = tree.height();
+    const size_t postRotationSize = tree.size();
 
     EXPECT_TRUE(preRotationHeight == 3);
     EXPECT_TRUE(preRotationSize == 4);
@@ -96,17 +96,17 @@ TEST(AvlTree, LeftRightInsertion) {
 }
 
 TEST(AvlTree, RightLeftInsertion) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(2);
-    tree->insert(1);
-    tree->insert(3);
-    tree->insert(5);
-    const int preRotationHeight = tree->height();
-    const size_t preRotationSize = tree->size();
-    tree->insert(4);
-    const int postRotationHeight = tree->height();
-    const size_t postRotationSize = tree->size();
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(3);
+    tree.insert(5);
+    const int preRotationHeight = tree.height();
+    const size_t preRotationSize = tree.size();
+    tree.insert(4);
+    const int postRotationHeight = tree.height();
+    const size_t postRotationSize = tree.size();
 
     EXPECT_TRUE(preRotationHeight == 3);
     EXPECT_TRUE(preRotationSize == 4);
@@ -115,29 +115,29 @@ TEST(AvlTree, RightLeftInsertion) {
 }
 
 TEST(AvlTree, EmptyRemoval) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    const bool successful = tree->remove(1);
+    const bool successful = tree.remove(1);
 
     EXPECT_FALSE(successful);
 }
 
 TEST(AvlTree, NonExistitantRemoval) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(1);
-    const bool successful = tree->remove(2);
+    tree.insert(1);
+    const bool successful = tree.remove(2);
 
     EXPECT_FALSE(successful);
 }
 
 TEST(AvlTree, OnlyNodeRemoval) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(1);
-    const bool successful = tree->remove(1);
-    const int height = tree->height();
-    const size_t size = tree->size();
+    tree.insert(1);
+    const bool successful = tree.remove(1);
+    const int height = tree.height();
+    const size_t size = tree.size();
 
     EXPECT_TRUE(successful);
     EXPECT_TRUE(height == 0);
@@ -145,15 +145,15 @@ TEST(AvlTree, OnlyNodeRemoval) {
 }
 
 TEST(AvlTree, RootRemoval) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(1);
-    tree->insert(2);
-    tree->remove(1);
+    tree.insert(1);
+    tree.insert(2);
+    tree.remove(1);
 
-    const int height = tree->height();
-    const size_t size = tree->size();
-    const int *value = tree->search(1);
+    const int height = tree.height();
+    const size_t size = tree.size();
+    const int *value = tree.search(1);
 
     EXPECT_TRUE(height == 1);
     EXPECT_TRUE(size == 1);
@@ -161,33 +161,44 @@ TEST(AvlTree, RootRemoval) {
 }
 
 TEST(AvlTree, MiddleRemoval) {
-    auto tree = std::make_unique<AVLTree<int>>();
+    AVLTree<int> tree;
 
-    tree->insert(1);
-    tree->insert(2);
-    tree->insert(3);
-    tree->insert(4);
-    tree->remove(3);
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(4);
+    tree.remove(3);
 
-    const int height = tree->height();
-    const size_t size = tree->size();
+    const int height = tree.height();
+    const size_t size = tree.size();
     EXPECT_TRUE(height == 2);
     EXPECT_TRUE(size == 3);
 }
 
-TEST(AvlTree, Iteration) {
-    auto tree = std::make_unique<AVLTree<int>>();
-    tree->insert(1);
-    tree->insert(2);
-    tree->insert(3);
+TEST(AvlTree, LeafRemoval) {
+    AVLTree<int> tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+    tree.remove(3);
 
-    auto iterator = tree->begin();
+    EXPECT_EQ(tree.size(), 2);
+    EXPECT_EQ(tree.search(3), nullptr);
+}
+
+TEST(AvlTree, Iteration) {
+    AVLTree<int> tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+
+    auto iterator = tree.begin();
     EXPECT_TRUE(*iterator == 1);
     iterator++;
     EXPECT_TRUE(*iterator == 2);
     iterator++;
     EXPECT_TRUE(*iterator == 3);
     iterator++;
-    EXPECT_TRUE(iterator == tree->end());
+    EXPECT_TRUE(iterator == tree.end());
 }
 // NOLINTEND
