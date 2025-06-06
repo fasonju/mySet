594ab665fbd4a89550501165c597cf940102cd3d
jasonfu <gimpact709@gmail.com>
2025-06-06 21:29:53 +0200

    better iteration

--- test/avl-tree.cpp
+++ test/avl-tree.cpp
@@ -139,9 +139,9 @@ TEST(AvlTree, LeafRemoval) {
 
 TEST(AvlTree, Iteration) {
     AVLTree<int> tree;
-    tree.insert(1);
-    tree.insert(2);
     tree.insert(3);
+    tree.insert(2);
+    tree.insert(1);
 
     auto it = tree.begin();
     EXPECT_EQ(*it++, 1);
