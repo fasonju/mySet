ee7c25986be5226f179f21f60f7ef0e5748bd72b
NickFris <n.frissen@student.tue.nl>
2025-06-06 15:18:27 +0200

    random level

--- tree/include/skiplist.hpp
+++ tree/include/skiplist.hpp
@@ -8,23 +8,20 @@ bool SkipList<T, Compare>::insert(T value) {
     return true;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-bool SkipList<T>::remove(const T& value) {
+template <typename T, typename Compare>
+bool SkipList<T, Compare>::remove(const T& value) {
 
     return true;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-T* SkipList<T>::search(const T& value) const {
+template <typename T, typename Compare>
+T* SkipList<T, Compare>::search(const T& value) const {
 
     return nullptr;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-T* SkipList<T>::max() const {
+template <typename T, typename Compare>
+T* SkipList<T, Compare>::max() const {
     // go all the way to the lowest layer of skiplist,
     // then run through until you reach a node that has no pointers out
     // return value of last node
@@ -32,42 +29,37 @@ T* SkipList<T>::max() const {
     return nullptr;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-T* SkipList<T>::min() const {
+template <typename T, typename Compare>
+T* SkipList<T, Compare>::min() const {
     // return the element after the dummy header
 
     return nullptr;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-bool SkipList<T>::contains(const T& value) const {
+template <typename T, typename Compare>
+bool SkipList<T, Compare>::contains(const T& value) const {
 
     return true;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-void SkipList<T>::clear() {
+template <typename T, typename Compare>
+void SkipList<T, Compare>::clear() {
     // discard previous linked list,
     // start with new dummy header with value negative inf
 
     // empty
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-bool SkipList<T>::empty() const {
+template <typename T, typename Compare>
+bool SkipList<T, Compare>::empty() const {
     // check if there is an element after dummy header
     // return true if there is none
 
     return true;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-size_t SkipList<T>::size() const {
+template <typename T, typename Compare>
+size_t SkipList<T, Compare>::size() const {
     // go all the way to the lowest layer of skiplist,
     // then run through and count the number of nodes
     // until you reach a node that has no pointers out
@@ -75,18 +67,18 @@ size_t SkipList<T>::size() const {
     return 0;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-int SkipList<T>::randomLevel() {
+template <typename T, typename Compare>
+int SkipList<T, Compare>::randomLevel() {
     // flip a coin, if heads, increment by one and flip again
     // if tails, stop and return
-
-    return 0;
+    int level = 1;
+    while (_dist(_gen) < 0.5 && level < _maxLevel)
+        ++level;
+    return level;
 }
 
-template <typename T>
-    requires std::totally_ordered<T>
-typename SkipList<T>::Node* SkipList<T>::createNode(const T& value, int level) {
+template <typename T, typename Compare>
+typename SkipList<T, Compare>::Node* SkipList<T, Compare>::createNode(const T& value, int level) {
     // idk
 
     return new Node(value, level);
