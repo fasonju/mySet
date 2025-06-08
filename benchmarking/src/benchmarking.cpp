9263fe2cbe287998020df6ce88edffe4bbef205d
jasonfu <gimpact709@gmail.com>
2025-06-08 05:04:35 +0200

    extra tests

--- benchmarking/src/benchmarking.cpp
+++ benchmarking/src/benchmarking.cpp
@@ -1,7 +1,7 @@
 #include "avl_tree/avl_tree.h"
 #include "container.h"
 #include "large_uniform.h"
-#include "log.h"
+#include "tiny_uniform.h"
 #include <array>
 #include <iostream>
 #include <set>
@@ -11,14 +11,43 @@ template <typename C, typename T, size_t N>
     requires Dontainer<C, T>
 void testInserts(const std::array<T, N> &dataset) {
     C container;
-    std::set<T> set;
     for (auto &datapoint : dataset) {
         container.insert(datapoint);
-        // set.insert(datapoint);
+    }
+
+    for (auto &datapoint : dataset) {
+        container.remove(datapoint);
     }
 
     std::cout << container.size() << "\n";
 }
+
+template <typename T, size_t N>
+void testInsertsSet(const std::array<T, N> &dataset) {
+    std::set<T> set;
+    for (auto &datapoint : dataset) {
+        set.insert(datapoint);
+    }
+
+    for (auto &datapoint : dataset) {
+        set.erase(datapoint);
+    }
+
+    std::cout << set.size() << "\n";
+}
+
 } // namespace
 
-int main() { testInserts<AVLTree<int>>(large_uniform); }
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        std::exit(1);
+    }
+    std::string arg = argv[1];
+    if (arg == "avl") {
+        std::cout << "AVL" << "\n";
+        testInserts<AVLTree<int>>(large_uniform);
+    } else {
+        std::cout << "Set" << "\n";
+        testInsertsSet(large_uniform);
+    }
+}
